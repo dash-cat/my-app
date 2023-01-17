dd559f91a1d88ad8fbd5151871357e3dd466e5b4
Gregoriiy <gregory.sudakov@btb.dev>
2023-01-18 03:59:24 +0600

    homePage

---
+++ src/HomeApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function HomeApp() {
+  return (
+    <div className="App">
+      <div>GOVNO GOVNO</div>
+    </div>
+  );
+}
+
+export default HomeApp;

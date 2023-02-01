4e45d4e00b84cd8f8231931c3d9970c14efd58d3
Gregoriiy <gregory.sudakov@btb.dev>
2023-02-01 13:28:10 +0600

    stars for 200

--- src/Stars.tsx
+++ src/Stars.tsx
@@ -16,16 +16,19 @@ class Stars extends React.Component {
     componentDidMount() {
       const canvasik = this.myRef?.current
       var ctx = canvasik?.getContext("2d");
+      function rand () { return Math.random() * 100}
         if (!ctx) { return }
         ctx.fillStyle = "rgb(0,0,0)";
         if (!canvasik) { return }
         ctx.fillRect (0, 0, canvasik.width, canvasik.height);
+        for (let i = 0; i < 100; i ++) {
+            ctx.fillStyle = "rgba(255, 255, 255, 1)";
+        ctx.fillRect (rand(), rand(), 2, 1);
 
         ctx.fillStyle = "rgba(255, 255, 255, 1)";
-        ctx.fillRect (30, 30, 2, 1);
-
-        ctx.fillStyle = "rgba(255, 255, 255, 1)";
-        ctx.fillRect (15, 70, 3, 1);
+        ctx.fillRect (rand(), rand(), 3, 1);
+        }
+        
     }
     render() {
       return <canvas id="tutorial" width="150" height="150" ref={this.myRef} />;

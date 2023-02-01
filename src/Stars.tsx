c5c0ab50a809a66b793be437ddbdbc80298d6cb3
Gregoriiy <gregory.sudakov@btb.dev>
2023-02-01 13:16:11 +0600

    added stars

--- src/Stars.tsx
+++ src/Stars.tsx
@@ -17,14 +17,15 @@ class Stars extends React.Component {
       const canvasik = this.myRef?.current
       var ctx = canvasik?.getContext("2d");
         if (!ctx) { return }
-        ctx.fillStyle = "rgb(70,130,20, .5)";
-        ctx.fillRect (10, 10, 50, 50);
+        ctx.fillStyle = "rgb(0,0,0)";
+        if (!canvasik) { return }
+        ctx.fillRect (0, 0, canvasik.width, canvasik.height);
 
-        ctx.fillStyle = "rgba(0, 90, 20, 0.5)";
-        ctx.fillRect (30, 30, 50, 50);
+        ctx.fillStyle = "rgba(255, 255, 255, 1)";
+        ctx.fillRect (30, 30, 2, 1);
 
-        ctx.fillStyle = "rgba(20, 90, 100, 0.5)";
-        ctx.fillRect (15, 70, 90, 90);
+        ctx.fillStyle = "rgba(255, 255, 255, 1)";
+        ctx.fillRect (15, 70, 3, 1);
     }
     render() {
       return <canvas id="tutorial" width="150" height="150" ref={this.myRef} />;

1de2c0e455c950cd0ef1d727c898d06461f6c012
Gregoriiy <gregory.sudakov@btb.dev>
2023-02-01 15:17:16 +0600

    save star

--- src/Stars.tsx
+++ src/Stars.tsx
@@ -7,6 +7,12 @@ import './Stars.css'
 
 //   );
 // }  
+
+type Star = {
+  fillStyle: string;
+  rect: [number, number, number, number]
+};
+
 class Stars extends React.Component {
     constructor(props: {}) {
       super(props);
@@ -14,25 +20,28 @@ class Stars extends React.Component {
     private myRef = React.createRef<HTMLCanvasElement>();
     componentDidMount() {
       const self = this;
+      const stars: Star[] = []
+      function rand () { return Math.random() * 10000}
       function animation () {
         const canvasik = self.myRef?.current
         var ctx = canvasik?.getContext("2d");
-        function rand () { return Math.random() * 10000}
-          if (!ctx) { return }
-           ctx.fillStyle = "rgb(0,0,0)";
-          if (!canvasik) { return }
-           ctx.fillRect (0, 0, canvasik.width, canvasik.height);
-          for (let i = 0; i < 10; i ++) {
-            ctx.fillStyle = "rgba(255, 255, 255, 1)";
-            ctx.fillRect (rand(), rand(), rand() / 100, rand() / 100);
-
-            ctx.fillStyle = "rgba(255, 255, 255, 1)";
-            ctx.fillRect (rand(), rand(), 25, 16);
+        if (!ctx) { return }
+        ctx.fillStyle = "rgb(0,0,0)";
+        if (!canvasik) { return }
+        ctx.fillRect (0, 0, canvasik.width, canvasik.height);
+        for (let star of stars) {
+          ctx.fillStyle = star.fillStyle
+          ctx.fillRect (...star.rect)
         }
         requestAnimationFrame(animation)
       }
       animation()
-        
+      for (let i = 0; i < 100; i ++) {
+        stars.push(
+          {fillStyle : "rgba(255, 255, 255, 1)", rect: [rand(), rand(), rand() / 100, rand() / 100]},
+          {fillStyle: "rgba(255, 255, 255, 1)", rect :[rand(), rand(), 25, 16]}
+        )
+      }
     }
     render() {
       return <canvas className='starina' id="tutorial" width="10000" height="10000" ref={this.myRef} />;
